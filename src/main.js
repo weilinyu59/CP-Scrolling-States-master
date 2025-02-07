e67db544e4f3602f19558dae558aee7265eec58c
WeiLin Yu <wyu58@ucsc.edu>
2025-02-07 15:34:49 -0800

    finish

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// Code Practice: Scrolling States
+// Name:Weilin Yu
+// Date: 2/7/2025
+
+'use strict'
+
+const config = {
+    parent: 'phaser-game',  // for info text
+    type: Phaser.WEBGL,     // for tinting
+    width: 400,
+    height: 300,
+    pixelArt: true,
+    zoom: 2,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [ Load, Play ]
+}
+
+const game = new Phaser.Game(config)
